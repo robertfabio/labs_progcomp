d9975cfcaaa5c1297c1fd8341e53ced5704f49e8
Fábio Gurgel <fabio.filho25453@alunos.ufersa.edu.br>
2025-09-08 21:55:10 -0300

    Lab4 fixa

---
+++ lab04-fixa01.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+void sorria();
+
+int main() {
+	
+	sorria();
+	sorria();
+	sorria();
+	cout << endl;
+
+	sorria();
+	sorria();
+	cout << endl;
+
+	sorria();
+	cout << endl;
+}
+
+void sorria() {
+	cout << "Sorria! ";
+}
