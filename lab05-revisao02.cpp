70532e3471c49724c57fc43ad8310728e024e2ad
Fábio Gurgel <fabio.filho25453@alunos.ufersa.edu.br>
2025-09-10 18:58:06 -0300

    Update lab05-revisao02.cpp

--- lab05-revisao02.cpp
+++ lab05-revisao02.cpp
@@ -1,23 +1,20 @@
 #include <iostream>
 using namespace std;
-int num;
-int f();
-int g();
-int main()
-{
-	num = 1;
-	cout << g() + f() + num << endl;
+
+double aumentarSalario(double);
+
+int main() {
+	system("chcp 1252 > nul");
+
+	cout << "Salário atual: R$";
+	double salarioAtual;
+	cin >> salarioAtual;
+	cout << "Salário ajustado para: R$" << aumentarSalario(salarioAtual);
 }
-int f()
-{
-	num = num + 3;
-	return num;
+
+double aumentarSalario(double aumento) {
+	
+	aumento = aumento * 1.15;
+
+	return aumento;
 }
-int g()
-{
-	num = 2;
-	return num;
-}/* Qual é a ordem de chamada das funções dentro da função principal? Ou
-seja, quem será chamada primeiro, f() ou g()? */// f() sera chamado primeiro, 4 + 2 + 2 = 8/* Qual é o resultado do programa? Ele seria o mesmo se invertêssemos a
-ordem das chamadas de f() e g() dentro da função principal?resultado = 8; sim, fica 12 alterando a chamada. *//* Existe alguma garantia de que as funções f() e g() serão sempre chamadas
-na ordem em que elas aparecem no programa?	Sim, por ser estruturado				*/
