deb45b98cd2b2232511b8f8763ccd4c45352bb37
Fábio Gurgel <fabio.filho25453@alunos.ufersa.edu.br>
2025-09-10 18:57:50 -0300

    Create funcoes.cpp

---
+++ funcoes.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+void Flexao(int n)
+{
+	cout << "VOCE VAI FAZER " << n << " FLEXOES" << endl;
+}
+void Abdominal(int n)
+{
+	cout << "VOCE VAI FAZER " << n << " ABDOMINAIS" << endl;
+}
