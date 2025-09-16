7562f2c9835ed7738152da555d94ff0faa5724ff
Fábio Gurgel <fabio.filho25453@alunos.ufersa.edu.br>
2025-09-16 12:27:39 -0300

    Create lab06-fix01.cpp

---
+++ lab06-fix01.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	int total = 0;
+	cout << "Inicialmente total = " << total << " (ZERO)\n";
+	total = 50;
+	cout << "Depois da 'atribuição' total = " << total << endl;
+	return 0;
+}
