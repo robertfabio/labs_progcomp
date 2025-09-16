615fa580c90ba77d0e3550a3a6d695240cb7813a
Fábio Gurgel <fabio.filho25453@alunos.ufersa.edu.br>
2025-09-16 12:27:53 -0300

    Create lab06-aprende04.cpp

---
+++ lab06-aprende04.cpp
@@ -0,0 +1,16 @@
+#include <iostream> 
+using namespace std;
+
+long long calculo(long long, long long);
+
+int main()
+{
+	long long resultado = 200530LL * 420800LL;
+	cout << "Direto: " << resultado << endl;
+	cout << "Função: " << calculo(200530LL, 420800LL) << endl;
+	return 0;
+}
+	long long calculo(long long a, long long b)
+{
+	return a * b;
+}
