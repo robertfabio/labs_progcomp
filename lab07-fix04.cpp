be82a913338044e70a97171770a486004eff60fd
Fábio Gurgel <fabio.filho25453@alunos.ufersa.edu.br>
2025-09-17 12:02:07 -0300

    Create lab07-fix04.cpp

---
+++ lab07-fix04.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Maça =" << "\t\t 20 Unidades" << endl;
+    cout << "Melancia =" << "\t 50 Unidades" << endl;
+    cout << "Ameixa =" << "\t 30 Unidades" << endl;
+    return 0;
+}
