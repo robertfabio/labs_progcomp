d08ecacb1ce8fcc5ea940193a1cdd8c99ebb0603
Fábio Gurgel <fabio.filho25453@alunos.ufersa.edu.br>
2025-09-17 12:02:11 -0300

    Create lab07-fix05.cpp

---
+++ lab07-fix05.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+short ligarBit(unsigned char);
+short desligarBit(unsigned char);
+short testarBit(unsigned char);
+
+
+int main() {
+
+	unsigned char bit = 0;
+	int pos
+
+}
