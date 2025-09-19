a62df9e5b899d4afdc0072fcea0a9a9f4f1f1d6c
Fábio Gurgel <fabio.filho25453@alunos.ufersa.edu.br>
2025-09-19 10:34:21 -0300

    Update lab07-fix05.cpp

--- lab07-fix05.cpp
+++ lab07-fix05.cpp
@@ -1,14 +1,41 @@
 #include <iostream>
 using namespace std;
 
-short ligarBit(unsigned char);
-short desligarBit(unsigned char);
-short testarBit(unsigned char);
-
+void ligarBit(unsigned char&, int);
+void desligarBit(unsigned char&, int);
+bool testarBit(unsigned char&, int );
 
 int main() {
 
 	unsigned char bit = 0;
-	int pos
+	int posicao = 0;
+	cout << "Ligar qual bit? " << endl;
+	cin >> posicao;
+	if (posicao >= 0 && posicao <= 7) {
+	}
+	else {
+		cout << "Posição inválida! Use 0 a 7." << endl;
+	}
+	ligarBit(bit, posicao);
+	cout << "Olhe qual bit voce ligou: " << int(bit) << endl;
+	cout << endl;
+	if (testarBit(bit, posicao)) {
+		cout << "O bit está ligado viiuuuuuuu" << endl;
+	}
+	cout << "Quer desligar qual bit?" << endl;
+	cin >> posicao;
+	desligarBit(bit, posicao);
+	cout << "Olhe qual bit voce desligou: " << int(bit) << endl;
+	cout << endl;
 
 }
+
+void ligarBit(unsigned char &valor, int posicao) {
+	valor |= (1 << posicao);
+};
+void desligarBit(unsigned char &valor, int posicao) {
+	valor &= ~(1 << posicao);
+};
+bool testarBit(unsigned char &valor, int posicao) {
+	return (valor & (1 << posicao)) != 0;
+};
