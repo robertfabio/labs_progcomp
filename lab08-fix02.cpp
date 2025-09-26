4c23cfd7d11d58ce586034bccd0a32895394b5c0
Fábio Gurgel <fabio.filho25453@alunos.ufersa.edu.br>
2025-09-26 12:44:31 -0300

    Create lab08-fix02.cpp

---
+++ lab08-fix02.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	cout << "Digite um ponto flutuante: ";
+	float valor; cin >> valor;
+	cout << "Notacao padrao: " << valor << endl;
+	cout << "Notacao cientifica: " << scientific << valor << endl;
+	cout << "Notacao fixa: " << fixed << valor << endl;
+
+}
