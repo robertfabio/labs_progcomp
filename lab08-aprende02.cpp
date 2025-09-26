18e5beac9bc2a2e23b37fa0a2e79f8334a594420
Fábio Gurgel <fabio.filho25453@alunos.ufersa.edu.br>
2025-09-26 12:44:47 -0300

    Create lab08-aprende02.cpp

---
+++ lab08-aprende02.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+float formBitFloat(int);
+
+int main() {
+}
+
+float formBitFLoat(float bit) {
+	 
+	
+
+
+}
+
+/*
+O	ponto flutuante	possui	uma	representação	binária	que	se	divide	em	mantissa,
+sinal	da	mantissa	e	expoente.	Os	bits	podem	ser	transformados	em	um	valor
+através	da	fórmula	v	=	s	*	2(e-127)	* (1	+	m). Descubra o	valor	ponto flutuante
+correspondente	a	seguinte	codificação	binária de	32	bits:
+1 1 0 0 1 0 0 0 0 0 1 1 0 0 0 1 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+*/
