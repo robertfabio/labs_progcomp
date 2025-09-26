c1f601637090c670cabd3db6df745997a4fa941a
Fábio Gurgel <fabio.filho25453@alunos.ufersa.edu.br>
2025-09-26 12:44:49 -0300

    Create lab08-aprende03.cpp

---
+++ lab08-aprende03.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+	cout << fixed << cout.precision(8);
+	cout << fixed << cout.precision(8);
+	cout << fixed << cout.precision(8);
+	cout << fixed << cout.precision(8);
+	cout << fixed << cout.precision(8);
+
+}
