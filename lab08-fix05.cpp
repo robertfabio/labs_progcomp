0164166a217dae4199252bf9240628ac70d40dfd
Fábio Gurgel <fabio.filho25453@alunos.ufersa.edu.br>
2025-09-26 12:44:41 -0300

    Create lab08-fix05.cpp

---
+++ lab08-fix05.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	float f1 = 24980154.845f;
+	cout << f1 << endl;
+	float f2 = 2e38f;
+	cout << f2 << endl;
+	double d1 = 293849384958473847.394;
+	cout << d1 << endl;
+	double d2 = 2e307;
+	cout << d2 << endl;
+
+}
