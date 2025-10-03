4bfbf0315ba4ad3d9baba0cc1791cf008fed776e
Fábio Gurgel <fabio.filho25453@alunos.ufersa.edu.br>
2025-10-03 17:54:07 -0300

    Create lab10-fix02.cpp

---
+++ lab10-fix02.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	int vetA[5]{ 10, 20, 30, 40, 50};
+	int vetB[5]{ 10, 20, 0, 0, 0 };
+	int vetC[5]{ 0, 0, 30, 0, 0 };
+
+	cout << "Vetor A: " << vetA[0] << " " << vetA[1] << " " << vetA[2] << " " << vetA[3] << " " << vetA[4] << endl;
+	cout << "Vetor B: " << vetB[0] << " " << vetB[1] << " " << vetB[2] << " " << vetB[3] << " " << vetB[4] << endl;
+	cout << "Vetor C: " << vetC[0] << " " << vetC[1] << " " << vetC[2] << " " << vetC[3] << " " << vetC[4] << endl;
+}
